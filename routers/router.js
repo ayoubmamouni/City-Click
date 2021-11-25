32edf26dfcfd95d496e480fb244cf94e618e4d39
Ayoub Mamouni <ayoub.mamouni@yandex.com>
2021-11-25 13:20:22 +0100

    add cities to local server

--- routers/router.js
+++ routers/router.js
@@ -1,36 +1,12 @@
 const router = require("express").Router();
 const City = require("../db/click");
-const axios = require("axios");
+const moroccoCities = require("../cities");
+
 router.get("/", async (req, res) => {
-  let citiesNames = [];
   try {
-    const data = await axios
-      .get(process.env.API_URL, {
-        headers: {
-          "X-Parse-Application-Id": process.env.APP_ID, // This is an application id
-          "X-Parse-REST-API-Key": process.env.API_KEY, // This is a REST API key
-        },
-      })
-      .then((response) => {
-        return response;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-    let citiesData = await data.data.results;
-    await citiesData.forEach((city) => {
-      citiesNames.push(city.name);
-    });
-    let moroccoCities = await citiesNames.sort();
-    await res.render("home", { cities: moroccoCities });
-
-    // let moroccoCities = await citiesNames.sort();
-    // res.render("home", { cities: moroccoCities });
+    res.render("home", { cities: moroccoCities });
   } catch (err) {
-    let errorMessage =
-      "Oops!! we can't get the cities from another server, maybe you have slow internet connection, Try again if you want ;)";
-    res.json({ error: errorMessage });
+    res.json({ error: "Oops!! we can't load the cities." });
   }
 });
 
